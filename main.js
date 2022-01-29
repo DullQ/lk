1870622c7d72dac109dad9b4cdb703521de08e20
DullQ <84604962+DullQ@users.noreply.github.com>
2022-01-28 18:44:59 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log ("my frisodlk js projedct");
+var first_name = "Ranman";
+var last_name = "KapLap";
+
+var full_name = first_name.concat(last_name);
+
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+console.log()
+
+var add = a + b ;
+console.log(add)
+
+var sub = a - b ;
+console.log(sub);
+
+var multiply = a * b ;
+console.log(multiply);
+
+var divide = a / b ;
+console.log(divide);
